752c6a8a1b36e3c0710249b2fb1f982ee4d97e27
vihangkale <vihangk64@gmail.com>
2024-05-11 13:56:27 +0530

    view items pop up implemeneted

---
+++ app/modal.js
@@ -0,0 +1,53 @@
+"use client";
+import { useSearchParams, usePathname } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+
+function Modal() {
+  const searchParams = useSearchParams();
+  const modal = searchParams.get("modal");
+  const src = searchParams.get("src");
+  const price = searchParams.get("price");
+  const name = searchParams.get("name");
+  const description = searchParams.get("description");
+  const pathname = usePathname();
+  return (
+    <>
+      {modal && (
+        <dialog className="fixed left-0 top-0 w-full h-full bg-black bg-opacity-50 z-50 overflow-auto backdrop-blur flex justify-center items-center">
+          <div className="m-auto relative rounded-lg modalContent w-full mx-0.5 bg-[#0c483e]">
+            <div className="flex flex-col h-full">
+              <Image
+                src={src}
+                className="w-full h-4/5 rounded-lg"
+                width={100}
+                height={200}
+              />
+              <div className="flex flex-col text-white p-3 h-1/5">
+                <div className="flex justify-between w-full items-center">
+                  <p className="text-base text-semibold">{name}</p>
+                  <p className="text-sm text-semibold">{price}</p>
+                </div>
+                {description && (
+                  <p className="text-white text-[13px] font-normal">
+                    {description}
+                  </p>
+                )}
+              </div>
+            </div>
+            <Link href={pathname}>
+              <button
+                type="button"
+                className="bg-[#282828ab] rounded-full p-2 text-white w-[40px] h-[40px] absolute top-[10px] right-[10px]"
+              >
+                &#10005;
+              </button>
+            </Link>
+          </div>
+        </dialog>
+      )}
+    </>
+  );
+}
+
+export default Modal;
--- app/page.js
+++ app/page.js
@@ -19,16 +19,25 @@ export default async function Home() {
   const banners = result?.banners;
   const categories = result?.categories;
   const formatText = (text) => text.replaceAll("%20", " ");
+  const formatPrice = (price) => {
+    const parsed = String(price);
+    const isLenghtMoreThan3 = parsed.length > 3;
+    if (isLenghtMoreThan3) {
+      const text = `${parsed.slice(0, -3)},${parsed.slice(-3)}`;
+      return text;
+    }
+    return price;
+  };
   return (
     <main className="min-h-screen pt-4 bg-[#0c483e]">
       <h1 className="text-3xl font-bold text-center text-white">Food Menu</h1>
       <div className="flex items-center gap-x-4 mt-4 overflow-auto px-4 py-2">
-        {categories?.map(({ _id, name, image_big }) => (
+        {categories?.map(({ _id, name, image_sm }) => (
           <Link href={`#${_id}`} key={_id}>
             <div className="flex flex-col gap-y-2" key={_id}>
               <Image
                 className="min-w-[100px] min-h-[70px] max-h-[70px] object-cover"
-                src={image_big}
+                src={image_sm}
                 alt="category"
                 width={100}
                 height={70}
@@ -54,33 +63,44 @@ export default async function Home() {
             </div>
           ))}
         </div>
-        {categories?.map(({ _id, name, items }) => (
+        {categories?.map(({ _id, name, items, image_sm }) => (
           <div key={_id} className="mt-3" id={_id}>
             <p className="text-2xl font-semibold text-white">
               {formatText(name)}
             </p>
             <div className="grid grid-cols-2 gap-4 mt-3">
-              {items?.map(({ _id, images, price }) => (
-                <div
-                  key={_id}
-                  className="flex flex-col h-[178.5px] item-card rounded-lg"
-                >
-                  <Image
-                    className="w-full max-h-[120px] rounded-lg"
-                    src={images[0]?.url}
-                    alt="item"
-                    width={188}
-                    height={120}
-                  />
-                  <div className="p-2">
-                    <p className="text-white font-semibold text-sm">
-                      {formatText(name)}
-                    </p>
-                    <hr className="border-[#ffffff1f]" />
-                    <p className="text-white font-semibold text-sm">{price}</p>
-                  </div>
-                </div>
-              ))}
+              {items?.map(({ _id, images, price, name, description }) => {
+                const src = images[0]?.url ? images[0]?.url : image_sm;
+                return (
+                  <Link
+                    href={`?modal=true&src=${src}&name=${formatText(
+                      name
+                    )}&price=${formatPrice(price)}&description=${description}`}
+                  >
+                    <div
+                      key={_id}
+                      className="flex flex-col h-[178.5px] item-card rounded-lg"
+                    >
+                      <Image
+                        className="w-full max-h-[120px] rounded-lg"
+                        src={src}
+                        alt="item"
+                        width={188}
+                        height={120}
+                      />
+                      <div className="p-2">
+                        <p className="text-white font-semibold text-sm">
+                          {formatText(name)}
+                        </p>
+                        <hr className="border-[#ffffff1f]" />
+                        <p className="text-white font-semibold text-sm">
+                          {formatPrice(price)}
+                        </p>
+                      </div>
+                    </div>
+                  </Link>
+                );
+              })}
             </div>
           </div>
         ))}
